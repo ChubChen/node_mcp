de6ddf45724b906f2fca98dc201ac6decb9c7264
ChubChen <CH@localhost.localdomain>
2015-05-21 16:32:55 +0800

    update code

--- Notify.js
+++ Notify.js
@@ -65,7 +65,7 @@ Notify.prototype.start = function()
 Notify.prototype.sendNotify = function()
 {
     var self = this;
-    self.sendJob = new CronJob('*/10 * * * * *', function () {
+    self.sendJob = new CronJob('*/5 * * * * *', function () {
         self.sendUntilEmpty();
     });
     self.sendJob.start();
@@ -81,15 +81,12 @@ Notify.prototype.sendUntilEmpty = function()
     async.waterfall([
         //10秒钟通知一次。并且取出所有队列消息并分组通知
         function (cb) {
-            var cursor = table.find({},{},[]).sort({customerId:1}).limit(20);
+            var cursor = table.find({},{},[]).sort({customerId:1}).limit(10);
             var userMap = new Object();
             var userOther = new Object();
             cursor.toArray(function (err, data) {
                 async.eachSeries(data, function(ticket, callback){
-                    self.getCmd(ticket, function(err, cmd){
-                        if(err){
-                            callback(err);
-                        }else{
+                     if(ticket.type == notifyType.TICKET){
                             var key = ticket.customerId +"_"+ data;
                             if(!userMap[key]){
                                 var array = new Array();
@@ -97,7 +94,7 @@ Notify.prototype.sendUntilEmpty = function()
                                 userMap[key] = array;
                                 var info = {};
                                 info.userId = ticket.customerId;
-                                info.cmd = cmd;
+                                info.cmd = 'N02';
                                 info.options = {
                                     hostname: ticket.ip,
                                     port: ticket.port,
@@ -116,17 +113,42 @@ Notify.prototype.sendUntilEmpty = function()
                                 }
                                 info.msgDigestType = msgDigestType;
                                 userOther[key] = info;
-                                //table.remove({_id: ticket._id}, {},function(err, data){
+                                table.remove({_id: ticket._id}, {},function(err, data){
                                     callback(err);
-                                //});
+                                });
                             }else{
                                 userMap[key].push(ticket.content);
-                               // table.remove({_id: ticket._id}, {},function(err, data){
+                                table.remove({_id: ticket._id}, {},function(err, data){
                                     callback(err);
-                               // });
+                                });
                             }
-                        }
-                    })
+                     }else{
+                         userMap[ticket._id] = ticket.content;
+                         var info = {};
+                         info.userId = ticket.customerId;
+                         info.cmd = self.getCmd(ticket);
+                         info.options = {
+                             hostname: ticket.ip,
+                             port: ticket.port,
+                             path: ticket.path,
+                             method: 'POST'
+                         };
+                         info.key = ticket.key;
+                         var msgDigestType = ticket.digestType;
+                         if(msgDigestType == undefined)
+                         {
+                             msgDigestType = digestType.getInfoById(digestType.trippleDes).headCode;
+                         }
+                         else
+                         {
+                             msgDigestType = digestType.getInfoById(msgDigestType).headCode;
+                         }
+                         info.msgDigestType = msgDigestType;
+                         userOther[ticket._id] = info;
+                         table.remove({_id: ticket._id}, {},function(err, data){
+                             callback(err);
+                         });
+                     }
                 }, function (err) {
                     cursor.count(function(err, count){
                         cb(null, userMap, userOther, count);
@@ -139,10 +161,9 @@ Notify.prototype.sendUntilEmpty = function()
         {
             if(count > 0){
                 for(var key in userMap ){
-                    var msgArray = userMap[key];
+                    var msgObj = userMap[key];
                     var info = userOther[key];
-                    log.info("开始向用户" + info.userId + "发送消息");
-                    self.sendMsg(info.options, info.msgDigestType, info.key, info.cmd, msgArray, 1, function(err, data){
+                    self.sendMsg(info.options, info.msgDigestType, info.key, info.userId, info.cmd, msgObj, 1, function(err, data){
                         cb(err, data);
                     });
                 }
@@ -158,41 +179,32 @@ Notify.prototype.sendUntilEmpty = function()
         }
     });
 }
-Notify.prototype.getCmd = function(msg, cb){
+Notify.prototype.getCmd = function(msg){
     var cmd = '';
-    if(msg.type == notifyType.TICKET)
-    {
-        cmd = "N02";
-
-    }
-    else if(msg.type == notifyType.GAME)
+    if(msg.type == notifyType.GAME)
     {
         cmd = "N03";
     }
     else if(msg.type == notifyType.TERM)
     {
-        if(msg.status == termStatus.ON_SALE){
+        if(msg.content.status == termStatus.ON_SALE){
             cmd = 'N04';
-        }else if(msg.status == termStatus.END){
+        }else if(msg.content.status == termStatus.END){
             cmd = 'N05';
-        }else if(msg.status == termStatus.SEND){
+        }else if(msg.content.status == termStatus.SEND){
             cmd = 'N06';
-        }else if(msg.status == termStatus.DRAW){
+        }else if(msg.content.status == termStatus.DRAW){
             cmd = 'N07'
-        }else if(msg.status == termStatus.SEAL){
+        }else if(msg.content.status == termStatus.SEAL){
             cmd = 'N08';
         }
-    }else{
-        cb(ec.E9000);
-        return;
     }
-    cb(null, cmd);
-    return
+    return cmd;
 }
 /**
  * 发送单个消息
  */
-Notify.prototype.sendMsg = function(options, msgDigestType, key, cmd, msgArray, tryCount, cb)
+Notify.prototype.sendMsg = function(options, msgDigestType, key, userId, cmd, msgObj, tryCount, cb)
 {
     var self = this;
     if(!options.hostname || !key || key.length == 0)
@@ -203,7 +215,7 @@ Notify.prototype.sendMsg = function(options, msgDigestType, key, cmd, msgArray, 
     async.waterfall([
         //根据业务过滤返回字段
         function(cb){
-            notifyControl.handle(msgArray, cmd, function (err, bodyNode) {
+            notifyControl.handle(msgObj, cmd, function (err, bodyNode) {
                 if(err){
                     cb(err);
                 }else{
@@ -215,6 +227,7 @@ Notify.prototype.sendMsg = function(options, msgDigestType, key, cmd, msgArray, 
         if(err){
             log.error(err);
         }else{
+            log.info("开始向用户" + userId + "发送消息");
             notifyUtil.send(options, msgDigestType, key, cmd, msg, function(err, data){
                 if(err)
                 {
