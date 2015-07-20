1ce6f9557e8c586c0bb63abe768e6133fb61d831
ChubChen <CH@localhost.localdomain>
2015-07-20 20:26:32 +0800

    update code

--- helper/JcLotTest.js
+++ helper/JcLotTest.js
@@ -53,7 +53,7 @@ LotTest.prototype.lotT51 = function(cb){
         },*/{
             gameCode:'T51', pType:'02', bType:'21', amount:400,
             multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201507142001|3,1;02|201507142003|1'
+            number:'02|201507201010|3,1;02|201507201009|1'
         }
         /*{
             gameCode:'T51', pType:'06', bType:'71', amount:200,
@@ -108,7 +108,7 @@ LotTest.prototype.lotT51 = function(cb){
 var lotTest = new LotTest();
 var count = 0;
 async.whilst(
-    function() { return count < 10},
+    function() { return count < 3},
     function(whileCb) {
         lotTest.lotT51(function(){
             count++;
