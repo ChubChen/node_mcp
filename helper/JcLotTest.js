09257edf6a641df030831613a15be0b90e37eaf2
chenpeng <chenpeng@ezcp.cn>
2015-11-23 20:33:03 +0800

    udpate teest

--- helper/JcLotTest.js
+++ helper/JcLotTest.js
@@ -25,43 +25,13 @@ LotTest.prototype.lotT51 = function(cb){
     var self = this;
     var bodyNode = {};
 
-    var orderNode = {outerId:digestUtil.createUUID(), amount:12000};
+    var orderNode = {outerId:digestUtil.createUUID(), amount:1200};
     var ticketsNode = [
-        {
-            gameCode:'T51', pType:'02', bType:'31', amount:400,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201511231015|3;02|201511231016|3,1;02|201511231017|3'
-        },
-        {
-            gameCode:'T51', pType:'02', bType:'33', amount:1000,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201511231015|3;02|201511231016|3,1;02|201511231017|3'
-        },
-        {
-            gameCode:'T51', pType:'03', bType:'411', amount:2200,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'03|201511231015|21;03|201511231016|10;03|201511231017|10;03|201511231018|10'
-        },
-        {
-            gameCode:'T51', pType:'04', bType:'620', amount:4000,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'04|201511231015|0;04|201511231016|1;04|201511231017|2;04|201511231018|1;04|201511231019|0;04|201511231020|11'
-        },
        {
             gameCode:'T51', pType:'05', bType:'46', amount:1200,
             multiple:1, outerId:digestUtil.createUUID(),
-            number:'05|201511231015|11;05|201511231016|13;05|201511231017|31;05|201511231018|31;'
-        },
-        {
-            gameCode:'T51', pType:'06', bType:'44', amount:800,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'01|201511231015|3;01|201511231016|3;04|201511231017|2;03|201511231018|11'
-        },
-         {
-         gameCode:'T51', pType:'06', bType:'34', amount:1400,
-         multiple:1, outerId:digestUtil.createUUID(),
-         number:'01|201511231015|3;01|201511231016|3;03|201511231017|11,01'
-         }
+            number:'05|201511231015|11;05|201511231016|13;05|201511231017|31;05|201511231018|31'
+        }
     ]
     orderNode.tickets = ticketsNode;
     bodyNode.order = orderNode;
