c14cc48c339c7249d619de9631e5401c7be6f763
chenpeng <chenpeng@ezcp.cn>
2015-11-23 20:25:15 +0800

    udpate teest

--- helper/JcLotTest.js
+++ helper/JcLotTest.js
@@ -7,7 +7,7 @@ var digestUtil = esut.digestUtil;
 
 var LotTest = function(){
     var self = this;
-    self.userId = 'Q0001';
+    self.userId = 'Q0002';
     self.userType = "CHANNEL";
     self.key = '135790';
     self.cmd = 'CT03';
@@ -25,71 +25,43 @@ LotTest.prototype.lotT51 = function(cb){
     var self = this;
     var bodyNode = {};
 
-    var orderNode = {outerId:digestUtil.createUUID(), amount:400};
-  /*  var ticketsNode = [
-        {
-            gameCode:'T51', pType:'06', bType:'21', amount:200,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201501010101|1;01|201501010102|1@3.15'
-        }
-    ]*/
-/*    var ticketsNode = [
-        {
-            gameCode:'T51', pType:'02', bType:'11', amount:200,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201501010101|1'
-        }
-    ];*/
+    var orderNode = {outerId:digestUtil.createUUID(), amount:12000};
     var ticketsNode = [
-        /*{
-            gameCode:'T51', pType:'02', bType:'11', amount:200,
+        {
+            gameCode:'T51', pType:'02', bType:'31', amount:400,
             multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201503183001|1,2,3'
-        },*/
-        /*{
-            gameCode:'T51', pType:'06', bType:'21', amount:800,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'03|201504105001|13,90;02|201504105002|3,1'
-        },*/{
-            gameCode:'T51', pType:'02', bType:'21', amount:400,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201511172036|3,1;02|201511172035|1'
-        }
-        /*{
-            gameCode:'T51', pType:'06', bType:'71', amount:200,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201504013008|0;02|201504013010|3;02|201504013011|3;01|201504013012|1;01|201504013013|1;01|201504013014|0;01|201504013015|1'
-        }
-      /* {
-            gameCode:'T51', pType:'02', bType:'21', amount:400,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201504013003|3,1;02|201504013004|1'
-        },*/
-        /*{
-            gameCode:'T51', pType:'02', bType:'21', amount:200,
-            multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201503183001|1;02|201503183003|1'
+            number:'02|201511231015|3;02|201511231016|3,1;02|201511231017|3'
         },
         {
-            gameCode:'T51', pType:'02', bType:'21', amount:200,
+            gameCode:'T51', pType:'02', bType:'33', amount:1000,
             multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201503183001|3;02|201503183003|3'
+            number:'02|201511231015|3;02|201511231016|3,1;02|201511231017|3'
         },
         {
-            gameCode:'T51', pType:'02', bType:'21', amount:200,
+            gameCode:'T51', pType:'03', bType:'411', amount:2200,
             multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201503183002|0,1;02|201503183003|0'
+            number:'03|201511231015|21;03|201511231016|10;03|201511231017|10;03|201511231018|10'
         },
         {
-            gameCode:'T51', pType:'02', bType:'21', amount:200,
+            gameCode:'T51', pType:'04', bType:'620', amount:4000,
             multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201503183002|3,0;02|201503183003|0'
+            number:'04|201511231015|0;04|201511231016|1;04|201511231017|2;04|201511231018|1;04|201511231019|0;04|201511231020|11'
+        },
+       {
+            gameCode:'T51', pType:'05', bType:'46', amount:1200,
+            multiple:1, outerId:digestUtil.createUUID(),
+            number:'05|201511231015|11;05|201511231016|13;05|201511231017|31;05|201511231018|31;'
         },
         {
-            gameCode:'T51', pType:'02', bType:'21', amount:200,
+            gameCode:'T51', pType:'06', bType:'44', amount:800,
             multiple:1, outerId:digestUtil.createUUID(),
-            number:'02|201503183002|1;02|201503183003|0'
-        }*/
+            number:'01|201511231015|3;01|201511231016|3;04|201511231017|2;03|201511231018|11'
+        },
+         {
+         gameCode:'T51', pType:'06', bType:'34', amount:1400,
+         multiple:1, outerId:digestUtil.createUUID(),
+         number:'01|201511231015|3;01|201511231016|3;03|201511231017|11,01'
+         }
     ]
     orderNode.tickets = ticketsNode;
     bodyNode.order = orderNode;
@@ -108,7 +80,7 @@ LotTest.prototype.lotT51 = function(cb){
 var lotTest = new LotTest();
 var count = 0;
 async.whilst(
-    function() { return count < 3},
+    function() { return count < 1},
     function(whileCb) {
         lotTest.lotT51(function(){
             count++;
